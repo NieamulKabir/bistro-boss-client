fbfc4d2631da174015bcf223c52a8aadcfb91c0a
NieamulKabir <kabir000079@gmail.com>
2023-12-23 11:24:50 +0600

    hook form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col md:flex-row-reverse">
@@ -13,42 +23,63 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
               <input
+                {...register("name", { required: true })}
                 type="text"
                 name="name"
                 placeholder="Name"
                 className="input input-bordered"
-                
               />
+              {errors.name && (
+                <span className="text-red-600 mt-1">
+                  This Field is Required
+                </span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
+                {...register("email", { required: true })}
                 type="email"
                 name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
               />
+              {errors.email && (
+                <span className="text-red-600 mt-1">
+                  This Field is Required
+                </span>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
               <input
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                })}
                 type="password"
                 name="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
               />
+              {errors.password && (
+                <span className="text-red-600 mt-1">
+                  This Field is Required
+                </span>
+              )}
             </div>
 
             <div className="form-control mt-6">
@@ -59,7 +90,10 @@ const SignUp = () => {
           <p className="text-center mb-8">
             {" "}
             <small>
-              Have an account? <Link className="text-orange-500 font-semibold" to="/login">Click Here</Link>
+              Have an account?{" "}
+              <Link className="text-orange-500 font-semibold" to="/login">
+                Click Here
+              </Link>
             </small>
           </p>
         </div>
