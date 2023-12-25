dd44c9dd919a30638337ff89d52db23db2ad0ec4
NieamulKabir <kabir000079@gmail.com>
2023-12-25 12:55:42 +0600

    use transtack query

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,6 +1,20 @@
-const useCart =email=>{
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
-    
-}
+const useCart = () => {
+  const { user } = useContext(AuthContext);
+
+  const { isLoading, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:5000/carts?email=${user.email}`
+      );
+      return response.json();
+    },
+  });
+  return [cart, isLoading];
+};
 
 export default useCart;
