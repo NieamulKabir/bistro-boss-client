1694971b5aa3c1df47877ce12718aad8044472dd
NieamulKabir <kabir000079@gmail.com>
2023-12-26 23:57:04 +0600

    cart update

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -5,16 +5,60 @@ const MyCart = () => {
   const [cart] = useCart();
   console.log(cart);
 
-  const total = cart.reduce((sum,item)=>item.price+sum,0)
+  const total = cart.reduce((sum, item) => item.price + sum, 0);
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | MyCart</title>
       </Helmet>
-      <div>
-        <h3 className="text-3xl"> Total Items: {cart.length}</h3>
-        <h3 className="text-3xl"> Total Price: ${total}</h3>
+      <div className="uppercase font-semibold flex justify-evenly items-center h-[60px] space-x-10 mt-10">
+        <h3 className="text-3xl bg-[#f0c688] px-3 py-1 rounded-lg"> Total Items: {cart.length}</h3>
+        <h3 className="text-3xl bg-[#f0c688] px-3 py-1 rounded-lg"> Total Price: ${total}</h3>
         <button className="btn btn-sm btn-warning">PAY</button>
+      </div>
+      {/* table  */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Food</th>
+              <th>Item Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={item?.image}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
