6580bc8e47ab55d141e555f60f9f96e0f6560a07
NieamulKabir <kabir000079@gmail.com>
2023-12-31 01:44:45 +0600

    add checkoutform

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -2,7 +2,7 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const Payment = () => {
     return (
-        <div>
+        <div className="w-full">
             <SectionTitle subHeading="Please Process" heading="Payment"></SectionTitle>
             <h1 className="text-3xl">Teka o teka twmi uira uira jaw</h1>
         </div>
