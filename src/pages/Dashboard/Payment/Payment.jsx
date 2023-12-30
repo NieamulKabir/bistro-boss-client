15b55127ae46ba7c067e73f355f25e9a1d68bcfa
NieamulKabir <kabir000079@gmail.com>
2023-12-31 01:59:31 +0600

    update payment system

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,12 +1,27 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_Stripe_Payment_Gateway);
 
 const Payment = () => {
-    return (
-        <div className="w-full">
-            <SectionTitle subHeading="Please Process" heading="Payment"></SectionTitle>
-            <h1 className="text-3xl">Teka o teka twmi uira uira jaw</h1>
-        </div>
-    );
+  return (
+    <div className="w-full">
+      <SectionTitle
+        subHeading="Please Process"
+        heading="Payment"
+      ></SectionTitle>
+      
+
+      <div className="w-1/2 mx-auto">
+      <h1 className="text-3xl my-6">Teka o teka twmi uira uira jaw</h1>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
